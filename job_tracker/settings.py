19f9f7d5f9e506de43bbae70a9a628c0c49c04cc
Timothy Moon <timhgmoon@gmail.com>
2022-04-18 18:03:06 -0700

    re-set debug=true to look for problem

--- job_tracker/settings.py
+++ job_tracker/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-3ax@9#z7q_x=kt^dwv^bp*1gw8y58%#%q_2(v%t_!e_%vmzm^0'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
