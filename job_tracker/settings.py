4f0aae8ba3bc85311a928f9b1bb74efbec91cb9d
Timothy Moon <timhgmoon@gmail.com>
2022-04-20 19:21:39 -0700

    added django-cors-headers

--- job_tracker/settings.py
+++ job_tracker/settings.py
@@ -41,17 +41,22 @@ INSTALLED_APPS = [
     'rest_framework.authtoken',
     'rest_framework',
     'job_api',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    "corsheaders.middleware.CorsMiddleware",
+    "django.middleware.common.CommonMiddleware",
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+#need to change once front-end is deployed
+CORS_ALLOW_ALL_ORIGINS = True
 
 ROOT_URLCONF = 'job_tracker.urls'
 
